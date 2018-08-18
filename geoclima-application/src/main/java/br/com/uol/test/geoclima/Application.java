000bf409fbf7d9cd5db9f77da4a598423503eb40
Caroline Lopes <clopesblack@gmail.com>
2018-08-18 12:46:26 -0300

    Initial commit.

---
+++ geoclima-application/src/main/java/br/com/uol/test/geoclima/Application.java
@@ -0,0 +1,24 @@
+package br.com.uol.test.geoclima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by Caroline Lopes on 09/08/18.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
